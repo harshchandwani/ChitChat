142ddf5b6ef5bfa18afea95ea1719449d997934d
Harsh Chandwani <harshchandwani50028@gmail.com>
2024-02-08 16:49:31 +0530

    Socket.io URL updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chitchat-4xrw.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
